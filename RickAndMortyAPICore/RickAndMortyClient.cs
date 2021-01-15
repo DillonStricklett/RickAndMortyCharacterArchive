26da11e3e220a552525be201a3f7de1da5e8c334
Dillon Stricklett <dillonstricklett@live.com>
2021-01-14 18:52:58 -0800

    Created a class library project to handle all of the api code.

---
+++ RickAndMortyAPICore/RickAndMortyClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace RickAndMortyAPICore
+{
+    /// <summary>
+    /// Client class to consume TheRickAndMortyAPI
+    /// Docs: https://rickandmortyapi.com/
+    /// </summary>
+    public class RickAndMortyClient
+    {
+        static readonly HttpClient client;
+    }
+}
