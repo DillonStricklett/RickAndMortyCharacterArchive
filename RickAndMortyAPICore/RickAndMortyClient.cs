f23dd7b182cb4e21427bf5ec1f531ada3e29c650
Dillon Stricklett <dillonstricklett@live.com>
2021-01-15 00:25:47 -0800

    Implemented a method to get a character by id (#4)

--- RickAndMortyAPICore/RickAndMortyClient.cs
+++ RickAndMortyAPICore/RickAndMortyClient.cs
@@ -17,32 +17,70 @@ namespace RickAndMortyAPICore
         /// <summary>
         /// Retrieve characters by name.
         /// </summary>
+        /// <param name="name">User searched name.</param>
+        /// <returns></returns>
+        public async Task<Character> GetCharactersByName(string name)
+        {
+            return await GetCharactersByNameOrId(name);
+        }
+
+        /// <summary>
+        /// Gets a character by their id
+        /// </summary>
+        /// <param name="id">User searched id</param>
+        /// <returns></returns>
+        public async Task<Character> GetCharacterById(int id)
+        {
+            return await GetCharactersByNameOrId(id.ToString());
+        }
+
+        /// <summary>
+        /// Retrieve characters by name or id.
+        /// </summary>
         /// <exception cref="HttpRequestException"></exception>
         /// <param name="name">User searched name.</param>
         /// <exception cref="ArgumentException">Thrown when character is not found.</exception>
         /// <returns></returns>
-        public async Task<Character> GetCharactersByName(string name)
+        private static async Task<Character> GetCharactersByNameOrId(string name)
         {
-            string url = $"https://rickandmortyapi.com/api/character/?name={name}";
-            HttpResponseMessage response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
+            int id;
+            bool isNum = int.TryParse(name, out id);
+            if (isNum)
             {
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<Character>(responseBody);
-            }
-            else if(response.StatusCode == HttpStatusCode.NotFound)
-            {
-                throw new ArgumentException($"{name} does not exist.");
+                string url = $"https://rickandmortyapi.com/api/character/{id}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Character>(responseBody);
+                }
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new ArgumentException($"{id} does not exist.");
+                }
+                else
+                {
+                    throw new HttpRequestException();
+                }
             }
             else
             {
-                throw new HttpRequestException();
+                string url = $"https://rickandmortyapi.com/api/character/?name={name}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Character>(responseBody);
+                }
+                else if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new ArgumentException($"{name} does not exist.");
+                }
+                else
+                {
+                    throw new HttpRequestException();
+                }
             }
-        }
-
-        public void GetCharacterById(int id)
-        {
-
         }
     }
 }
