7cf53ecdf7627a55333575a6b8d60ecd24f447db
Dillon Stricklett <dillonstricklett@live.com>
2021-01-14 19:14:31 -0800

    Implemented a method that searches for charachters by name.

--- RickAndMortyAPICore/RickAndMortyClient.cs
+++ RickAndMortyAPICore/RickAndMortyClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace RickAndMortyAPICore
 {
@@ -9,6 +10,35 @@ namespace RickAndMortyAPICore
     /// </summary>
     public class RickAndMortyClient
     {
-        static readonly HttpClient client;
+        static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Retrieve characters by name.
+        /// </summary>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<string> GetCharactersByName(string name)
+        {
+            try
+            {
+                string url = $"https://rickandmortyapi.com/api/character/?name={name}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                throw ex;
+            }
+        }
+
+        public void GetCharacterById(int id)
+        {
+
+        }
     }
 }
